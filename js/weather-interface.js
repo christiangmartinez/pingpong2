229ce9880dffe4b2bd405e883417478e07a7d047
Andy Malkin and Christian Martinez <andy@>
2017-04-17 13:54:36 -0700

    add promise

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,12 +1,15 @@
 var apiKey = "e5f7039e049621f0bdb0632c7342ecbb";
 
-$(function() {
+$(document).ready(function() {
   $('#weather-location').click(function() {
     var city = $('#location').val();
     $('#location').val("");
-    $('.showWeather').text("The city you have chosen is " + city + ".");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey) .then(function(response) {
       $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + " %");
+    });
+    .fail(function(error) {
+      $('.showWeather').text(error.response.JSON.message);
     });
   });
 });
