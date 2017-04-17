80dc04c0e14342063729204a28a577b6227c6e24
Andy Malkin and Christian Martinez <christian@>
2017-04-17 09:58:41 -0700

    add dev/production variable and dependency

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var utilities = require('gulp-util');
+
+var buildProduction = utilities.env.production;
 
 gulp.task('jsBrowserify', ['concatInterface'], function() {
   return browserify({entries: ['./tmp/allConcat.js']})
@@ -15,10 +18,18 @@ gulp.task('minifyScripts', ['jsBrowserify'], function() {
   return gulp.src('./build/js/app.js')
   .pipe(uglify())
   .pipe(gulp.dest('./build/js'));
-})
+});
 
 gulp.task('concatInterface', function() {
   return gulp.src(['./js/*-interface.js'])
   .pipe(concat('allConcat.js'))
   .pipe(gulp.dest('./tmp'));
-})
+});
+
+gulp.task('build', function() {
+  if (buildProduction) {
+    gulp.start('minifyScripts');
+  } else {
+    gulp.start('jsBrowserify');
+  }
+});
